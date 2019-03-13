5767403ecb619e400917feaafe1222b9e22f4687
Anurag Devanapally <anurag.devanapally@gmail.com>
2019-03-12 21:45:28 -0700

    Update unitytest with fixes...
    - Update error checking in unitytest.
    - Convert test results path to absolute value.
    This is to avoid a Unity glitch where it always expects a relative path
    - Allow processing of default TestResults file if user does not explicitly specify one.
    - Log cmd_line in ProcessRun; Log UnityResults only if found in UnityTest.

--- andeart/lullapy/unitytest.py
+++ andeart/lullapy/unitytest.py
@@ -1,11 +1,15 @@
 import argparse
+import os
 from xml.etree import ElementTree
 
+from andeart.lullapy.easypath import EasyPath
 from andeart.lullapy.processrun import ProcessRunner
 from andeart.lullapy.shyprint import LogLevel, Logger
 
 
 class UnityTester:
+    test_results_file_pattern = "TestResults-*.xml"
+
 
     # noinspection SpellCheckingInspection
     def __init__(self, silent = False):
@@ -43,7 +47,8 @@ class UnityTester:
             self.__exit_with_error(1, "Unity project that contains tests was not provided.", parser.format_help())
 
         if self.__results_path is None:
-            self.__logger.log("No tests result output file was provided. Test results will not be saved.",
+            self.__logger.log("Explicit test-results output file was not provided. Test results will be saved to the "
+                              "default file.",
                               LogLevel.WARNING)
 
         # Initialize processrun
@@ -53,14 +58,12 @@ class UnityTester:
     def run_tests(self):
         result = self.__run_unity_tests(self.__unity_path, self.__project_path, self.__test_mode, self.__results_path)
 
-        # self.__logger.log("Test Runner results:\n", LogLevel.WARNING)
-        # with open(self.__results_path) as f:
-        #     content = f.readlines()
-        # content = [line.strip() for line in content]
-        # for line in content:
-        #     self.__logger.log(line)
-
-        self.__log_unity_results(self.__results_path)
+        if self.__results_path is not None and EasyPath.is_file(self.__results_path):
+            self.__log_unity_results(self.__results_path)
+        else:
+            latest_results = self.__get_latest_test_results(self.__project_path)
+            if latest_results is not None:
+                self.__log_unity_results(EasyPath.get_absolute_path(latest_results))
 
         if result.status != 0:
             self.__exit_with_error(result.status, "Unity TestRunner tests were not run successfully.")
@@ -71,14 +74,31 @@ class UnityTester:
     def __run_unity_tests(self, unity_app_path, project_path, test_mode, results_path):
         self.__logger.log_linebreaks(2)
         self.__logger.log("Running Unity TestRunner tests...", LogLevel.WARNING)
+
+        if not EasyPath.is_file(unity_app_path):
+            self.__exit_with_error(1, f"Unity app path: {unity_app_path} : is not an executable.")
+
+        if not EasyPath.is_dir(project_path):
+            self.__exit_with_error(1, f"Unity project path: {project_path} : is not a project directory.")
+
         cmd_line = f"{unity_app_path} -batchmode -runTests -projectPath {project_path} -testPlatform {test_mode}"
         if results_path is not None:
             cmd_line += f" -testResults {results_path}"
+
         return self.__process_run.run_line(cmd_line)
 
 
+    def __get_latest_test_results(self, project_path):
+        # If we've let Unity auto-generate the TestResults file, we simply select the most recently modified one.
+        files = EasyPath.glob(project_path, self.test_results_file_pattern)
+        files = sorted(files, key = os.path.getmtime, reverse = True)
+        latest_file = next(iter(files), None)
+        self.__logger.log("Most recent TestResults file found at: " + str(latest_file), LogLevel.WARNING)
+        return latest_file
+
+
     def __log_unity_results(self, results_path):
-        file = ElementTree.parse(results_path)
+        file = ElementTree.parse(str(results_path))
         file_root = file.getroot()
         # for test_case in file_root.findall("test-case"):
         #     self.__logger.log(test_case)
